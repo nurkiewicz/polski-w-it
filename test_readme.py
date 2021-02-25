6f1a565923daafc8b83cc47f1379e8bf0e3f9856
Tomasz Nurkiewicz <tnr@devskiller.com>
2021-02-25 10:44:57 +0100

    CI test making sure that dictionary doesn't contain untranslateable tests

--- test_readme.py
+++ test_readme.py
@@ -5,17 +5,12 @@ import pytest
 
 @pytest.fixture
 def readme():
-    with open("README.md", "r", encoding="utf-8") as myfile:
-        return [line for line in myfile.readlines() if line[0] == '|']
+    with open("README.adoc", "r", encoding="utf-8") as readme:
+        return [line for line in readme.readlines() if line[0] == '|']
 
 
 def english_term(row):
     return row[1:row[1:].find('|')].strip()
-
-
-@pytest.mark.skip(reason="TODO: skipped after migrating to AsciiDoc")
-def test_all_rows_in_table_equal_length(readme):
-    assert len(set([len(row) for row in readme])) == 1
 
 
 @pytest.mark.skip(reason="TODO: skipped after migrating to AsciiDoc")
@@ -24,10 +19,8 @@ def test_terms_are_sorted(readme):
     assert all_english_terms == sorted(all_english_terms)
 
 
-@pytest.mark.skip(reason="TODO: skipped after migrating to AsciiDoc")
-def test_columns_are_aligned(readme):
-    def column_position(row):
-        return [m.start() for m in re.finditer('|', row)]
-
-    column_position_in_each_line = [str(column_position(line)) for line in readme]
-    assert len(set(column_position_in_each_line)) == 1
+def test_does_not_contain_untranslateble_words(readme):
+    with open("untranslatable.adoc", "r", encoding="utf-8") as untranslatable:
+        untranslateble = [line[1:].strip() for line in untranslatable.readlines() if re.fullmatch('^\\| [a-z ]+\n$', line)]
+    for line in readme:
+        assert line[1:].strip() not in untranslateble
