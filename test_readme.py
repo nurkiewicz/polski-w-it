d66a610ae9d8ba4f5f1ce3ce3582b9f05d315a95
Tomasz Nurkiewicz <tnr@devskiller.com>
2021-02-25 15:19:42 +0100

    Testing alphabetical order

--- test_readme.py
+++ test_readme.py
@@ -6,17 +6,17 @@ import pytest
 @pytest.fixture
 def readme():
     with open("README.adoc", "r", encoding="utf-8") as readme:
-        return [line for line in readme.readlines() if line[0] == '|']
+        return [line for line in readme.readlines()]
 
 
 def english_term(row):
     return row[1:row[1:].find('|')].strip()
 
 
-@pytest.mark.skip(reason="TODO: skipped after migrating to AsciiDoc")
 def test_terms_are_sorted(readme):
-    all_english_terms = [english_term(line) for line in readme[2:]]
-    assert all_english_terms == sorted(all_english_terms)
+    all_english_terms = [tup[1][1:].strip() for tup in zip(readme, readme[1:])
+                         if tup[0].strip() == '' and tup[1].startswith('| ')]
+    assert list(all_english_terms) == sorted(all_english_terms)
 
 
 def test_does_not_contain_untranslateble_words(readme):
